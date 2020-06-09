795ed6e49aceb566ae2f841bcd5d158b5095d554
Kay <kimjh@bawi.org>
2020-06-09 19:09:37 +0900

    fix image layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import ViewingRoom from './component/ViewingRoom/ViewingRoom';
 export default function App() {
   return (
     <div className="App">
-      <ViewingRoom src={`${process.env.PUBLIC_URL}/image1.jpg`} size={[4632, 3112]} />
+      {/* <ViewingRoom src={`${process.env.PUBLIC_URL}/image1.jpg`} size={[4632, 3112]} /> */}
+      <ViewingRoom src={`${process.env.PUBLIC_URL}/image1.jpg`} />
     </div>
   );
 }
--- src/component/ViewingRoom/ViewingRoom.tsx
+++ src/component/ViewingRoom/ViewingRoom.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+
 import './ViewingRoom.scss';
 
 const useWindowSize = () => {
@@ -15,22 +16,66 @@ const useWindowSize = () => {
   return size;
 };
 
-export default function ViewingRoom({ src, size }: { src: string, size: number[] }) {
+export default function ViewingRoom({ src }: { src: string }) {
+  const [imgDimension, setImgDimension] = React.useState<number[]>([]);
+  const [containerDimension, setContainerDimension] = React.useState<number[]>([]);
   const [innerWidth, innerHeight] = useWindowSize();
-  const [width, height] = size;
-  const isLandscape: boolean = width > height;
+
+  React.useEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImgDimension([img.width, img.height, img.width / img.height]);
+    };
+    img.src = src;
+  }, [src]);
+
+  React.useEffect(() => {
+    console.log(containerDimension);
+  }, [containerDimension]);
+
+  React.useEffect(() => {
+    if (innerWidth >= 600) {
+      setContainerDimension([innerWidth * 0.5, innerHeight * 0.6,
+        (innerWidth / innerHeight) * (0.5 / 0.6)]);
+    } else {
+      setContainerDimension([innerWidth * (2 / 3), innerHeight * 0.6,
+        (innerWidth / innerHeight) * (10 / 9)]);
+    }
+  }, [innerWidth, innerHeight]);
 
   return (
     <Grid container className="container">
       <Grid item xs={2} sm={3} />
-      <Grid container direction="column" item xs={8} sm={6}>
+      <Grid container direction="column" item xs={8} sm={6} id="max-height">
         <Grid item className="heightGrid" />
-        <Grid container direction="column" item xs className="imageContainer">
-          <img src={`${process.env.PUBLIC_URL}/lantern.png`} alt="lantern" className="lanternOverlay" style={{ maxWidth: innerWidth / 4, left: 0 }} />
-          <img src={`${process.env.PUBLIC_URL}/lantern.png`} alt="lantern" className="lanternOverlay" style={{ maxWidth: innerWidth / 4, right: 0 }} />
-          <Grid item className="crop">
-            <img src={src} alt="ViewingRoomImage" id="viewing-image" />
-          </Grid>
+        <Grid container item xs className="widthContainer">
+          {imgDimension[0] > imgDimension[1]
+            ? (
+              <>
+                <img src={`${process.env.PUBLIC_URL}/lantern.png`} alt="lantern" className="lanternLandscape" style={{ maxWidth: innerWidth / 4, left: 0 }} />
+                <img src={`${process.env.PUBLIC_URL}/lantern.png`} alt="lantern" className="lanternLandscape" style={{ maxWidth: innerWidth / 4, right: 0 }} />
+              </>
+            ) : (
+              <img src={`${process.env.PUBLIC_URL}/lantern.png`} alt="lantern" className="lanternPortrait" style={{ maxWidth: innerWidth / 4 }} />
+            )}
+          <div className="imageContainer">
+            {containerDimension[2] > imgDimension[2]
+              ? (
+                <img
+                  src={src}
+                  alt="ViewingRoomImage"
+                  style={{ height: containerDimension[1] }}
+                  className="image"
+                />
+              ) : (
+                <img
+                  src={src}
+                  alt="ViewingRoomImage"
+                  style={{ width: containerDimension[0] }}
+                  className="image"
+                />
+              )}
+          </div>
         </Grid>
         <Grid item className="heightGrid" />
       </Grid>
